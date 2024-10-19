35015d348b60dbba6cb6f8329085aac67c1b19ac
ArseniyFrolov <arsik.frol.555@gmail.com>
2024-10-19 14:03:24 +0500

    feat: WeeklyDigest component

--- src/components/WeeklyDigest/WeeklyDigest.tsx
+++ src/components/WeeklyDigest/WeeklyDigest.tsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import styles from './styles.module.css'
 
+import weeklyDigest from '../../images/weeklyDigest.png'
+import email from '../../images/email.png'
+
 type Props = {}
 
 const WeeklyDigest = React.memo(function WeeklyDigest(props: Props) {
     return (
-        <div className={styles.wrapper}>WeeklyDigest</div>
+        <div className={styles.wrapper}>
+            <div className={styles.container}>
+                <div className={styles.leftContent}>
+                    <img draggable='false' src={weeklyDigest} />
+                </div>
+                <div className={styles.rightContent}>
+                    <div className={styles.title}>Join Our Weekly <br />Digest</div>
+                    <div className={styles.desc}>Get Exclusive Promotions & Updates Straight To Your Inbox.</div>
+                    <div className={styles.btn}>
+                        <input type="email" placeholder='Enter your email here' />
+                        <div className={styles.btnSubscridle}>
+                            <img draggable='false' src={email} alt="" />
+                            Subscribe
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 })
 
